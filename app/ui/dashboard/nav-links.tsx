8ca347f7317c7c5befd695bb51fecd019d1d8ce7
kaksv <kakoozavian@gmail.com>
2023-12-25 15:06:26 +0300

    first commit

---
+++ app/ui/dashboard/nav-links.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import {
+  UserGroupIcon,
+  HomeIcon,
+  CalendarDaysIcon,
+  DocumentDuplicateIcon,
+  BriefcaseIcon
+} from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: 'Home', href: '/dashboard/base', icon: HomeIcon },
+  {
+    name: 'Bounties',
+    href: '/dashboard/invoices',
+    icon: BriefcaseIcon,
+  },
+  { name: 'Events', href: '/dashboard/events', icon: CalendarDaysIcon },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 shadow-xl p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 shadow-sm text-blue-600 ' : pathname === link.href,
+              },
+            )}
+          >
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
