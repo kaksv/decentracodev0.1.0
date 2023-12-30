0a0b6275d40acfa098ccbb02079c0b71a6b998ad
kaksv <kakoozavian@gmail.com>
2023-12-30 21:23:30 +0300

    care to be taken

--- app/config/supabaseconfig.ts
+++ app/config/supabaseconfig.ts
@@ -1,11 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://sdnsnvaxabsvaapyspkw.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkbnNudmF4YWJzdmFhcHlzcGt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEzMTU5NTgsImV4cCI6MjAxNjg5MTk1OH0.Kt6o-LSQP-eR_NhWJ0_f374sCNWy4fmPESziK5KwMPc";
 
 const supabase = createClient(
-    supabaseUrl!, 
-    supabaseKey!
+    supabaseUrl, 
+    supabaseKey
     );
 
 export default supabase;
