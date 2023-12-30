fbd946e54c4c8b634fd241793165bf100cde8dca
kaksv <kakoozavian@gmail.com>
2023-12-30 21:17:58 +0300

    challenges are viewed

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,58 +1,80 @@
-// import supabase from '@/app/lib/supabase';
 
-import CardWrapper from '@/app/ui/dashboard/cards';
-// import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { lusitana } from '@/app/ui/fonts';
-// import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
-import { Suspense } from 'react';
-import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 
-export const revalidate = 0;
 
-export default async function Page() {
-    // const { data } = await supabase.from("postst").select();
-    // const revenue = await fetchRevenue();
-    // const latestInvoices = await fetchLatestInvoices();
-    // const {
-    //     numberOfInvoices,
-    //     numberOfCustomers,
-    //     totalPaidInvoices,
-    //     totalPendingInvoices
-    // } = await fetchCardData();
+// import { createClient } from '@supabase/supabase-js';
 
+import supabase from '@/app/config/supabaseconfig';
+
+export default async function Page () {
+  // const [bounties, setBounties] = useState<any>();
+  // const [loading, setLoading] = useState<boolean>(true);
+  // const [bounty, setBounty] = useState({ bounty_name: "", bounty_description: "", bounty_prize:""}) 
+  // const { bounty_name, bounty_description, bounty_prize} = bounty
+
+  // useEffect(() =>{
+  //   fetchBounties();
+  //   // setLoading(false);
+  // }, [])
+
+
+  // async function createBounty(){
+  //   await supabase.from("bounty").insert([
+  //     bounty_name, bounty_description, bounty_prize
+  //   ]).single()
+  //   setBounty({ bounty_name: "", bounty_description: "", bounty_prize: ""})
+  //   fetchBounties()
+  // }
+
+  
+    // <div className="">
+      {/* <input
+        placeholder="Name of the challenge/bounty"
+        value={bounty_name}
+        onChange={e => setBounties({ ... bounty, bounty_name: e.target.value })}
+      />
+      <input
+        placeholder="A description of about the challenge or bounty"
+        value={bounty_description}
+        onChange={e => setBounties({ ... bounty, bounty_description: e.target.value })}
+      />
+      <input
+        placeholder="Explanation of how the bounty shall be given out"
+        value={bounty_prize}
+        onChange={e => setBounties({ ... bounty, bounty_prize: e.target.value })}
+      /> */}
+      {/* <button className='bg-blue-500 text-white' onClick={createBounty}> Create challenge</button> */}
+      {
+      
+    }
+      {/* : {
+        bounties.map(bounty =>(
+          <div key={bounty.id}>
+            <h3>{bounty.bounty_name}</h3>
+            <p>{bounty.bounty_description}</p>
+            <p>{bounty.bounty_prize}</p>
+          </div>
+        ))
+      } */}
+    // </div>
+    const { data: bounties } = await supabase
+      .from("bounties")
+      .select('bounty_id, bounty_name, bounty_description, bounty_prize')
+    
+    
     return (
-        <main>
-            <h1 className={ `${lusitana.className} mb-4 text-x1 md:text-2x1 `}>
-                Dashboard
-                {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
-            </h1>
-            {/* <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                <Suspense fallback={<CardsSkeleton />}>
-                    <CardWrapper />
-                </Suspense> */}
-                {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-                <Card title="pending" value={totalPendingInvoices} type="pending" />
-                <Card title = "Total Invoices" value={numberOfInvoices} type="invoices" />
-                <Card
-                    title="Total Cusomers"
-                    value={numberOfCustomers}
-                    type="customers"
-                    /> */}
-               
-            {/* </div> */}
-            {/* <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8"> */}
-                {/* <RevenueChart revenue={revenue}  /> */}
-                {/* <Suspense fallback={<RevenueChartSkeleton />}>
-                    <RevenueChart />
-                </Suspense>
-                <Suspense fallback={<LatestInvoicesSkeleton/>} */}
-                {/* > */}
-                    {/* <LatestInvoices />
-                </Suspense> */}
-                {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
-            {/* // </div> */}
-        </main>
-    );
+      <>
+  
+
+     
+      {bounties?.map(bounty => (
+        <div className='p-4 border-b border-gray-400' key={bounty.bounty_id}>
+          <h1 className='text-2xl font-bold mb-4 '>{bounty.bounty_name}</h1>
+          <p className='p-2 text-ml'>{bounty.bounty_description}</p>
+          <p className='p-2 text-xl'>${bounty.bounty_prize}</p>
+        </div>
+      ))}
+      </>
+    )
+
+  
 }
