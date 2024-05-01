658da37381bd13402bb8d3bdb93be5f36363794b
Kakooza Vianey <kakoozavian@gmail.com>
2024-05-01 13:31:05 +0300

    Wit authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,26 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-import { Metadata } from "next";
+import { Metadata } from 'next';
 
 import { Web3Modal } from '@/context/Web3Modal';
-// import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider } from '@clerk/nextjs';
 
 export const metadata: Metadata = {
-  title: "Decentracode",
-  description: "A conduit for developers into web3",
-}
+  title: 'Decentracode',
+  description: 'A conduit for developers into web3',
+};
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    // <ClerkProvider>
-     <html lang="en">
-       <body className={`${inter.className} antialiased`}>
-      
-         <Web3Modal> {children} </Web3Modal>
-        
-         </body>
-     </html>
-    // </ClerkProvider>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>
+          <Web3Modal> {children} </Web3Modal>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
